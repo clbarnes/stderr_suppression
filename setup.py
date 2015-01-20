ee1a96c540a36fa323e5ff4cd6b68a8877e02a77
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2015-01-20 18:05:01 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='stderr_suppression',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/clbarnes/stderr_suppression',
+    license='New BSD',
+    author='clbarnes',
+    author_email='cbarnes@mrc-lmb.cam.ac.uk',
+    description=''
+)
