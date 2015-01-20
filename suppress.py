29bdf6fe1978cbf4ef8859bb23885106391b5258
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2015-01-20 17:57:22 +0000

    Added suppression code

---
+++ suppress.py
@@ -0,0 +1,29 @@
+import re
+import sys
+
+
+class NullErr():
+    REGEX = r"({})|(^\s$)"
+
+    def __init__(self, regex_str):
+        self.stderr = sys.stderr
+        self.regex = re.compile(NullErr.REGEX.format(regex_str))
+
+    def write(self, s):
+        if self.regex.match(s):
+            pass
+        else:
+            self.stderr.write(s)
+
+
+class WarningSuppressor():
+    def __init__(self, regex_str):
+        self.null_err = NullErr(regex_str)
+        self.std_err = sys.stderr
+
+    def __enter__(self):
+        sys.stderr = self.null_err
+        return self
+
+    def __exit__(self, *args):
+        sys.stderr = self.std_err
